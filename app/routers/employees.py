a4878fa0d1d7ec79dbb02bd491fcdaa13060a0a3
Nakkian <164665428+Nakkian@users.noreply.github.com>
2025-10-04 16:40:38 -0700

    Delete app/routers/employees.py

--- app/routers/employees.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends
-from ..auth import require_api_key
-
-router = APIRouter(
-    prefix="/employees",
-    tags=["employees"],
-    dependencies=[Depends(require_api_key)]   # <-- all endpoints in this file now require key
-)
