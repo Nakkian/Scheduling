b5cc880e27dea5ef5bfbb6f39581849c0cea94a9
Nakkian <164665428+Nakkian@users.noreply.github.com>
2025-10-04 18:47:34 -0700

    Create NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+// src/pages/NotFound.jsx
+export default function NotFound() {
+  return (
+    <section className="stack">
+      <h2>404</h2>
+      <p>Page not found.</p>
+    </section>
+  );
+}
